3bdfd8260964f8ec6887db326341d74826a25420
Sofiane El Marhani <s.elmarhani.ext@teamsystem.com>
2020-06-04 13:40:32 +0200

    refactor(console): move tau-prolog logic

--- src/app.js
+++ src/app.js
@@ -1,50 +1,11 @@
-const MAX_RESOLUTION_STEPS = 10000;
-const MODULES = ":- use_module(library(lists)).";
-const session = pl.create(MAX_RESOLUTION_STEPS);
-session.consult(MODULES);
-
-function query(query) {
-  session.query(query);
-  let answers_list = [];
-  let current_answer = true;
-  while (current_answer) {
-    session.answer((ans) => {
-      current_answer = ans;
-      // console.log(ans, pl.format_answer(ans));
-      answers_list.push(pl.format_answer(ans));
-    });
-  }
-  return answers_list;
-}
-
 var app = new Vue({
   el: "#app",
   data: () => ({
-    knowledgeBase: [],
-    session,
-    pl,
+    mazeobj: { string: "" },
   }),
   watch: {},
-  beforeCreate() {
-    fetch("src/prolog-scripts/antenati.pl")
-      .then((res) => res.text())
-      .then((res) => {
-        this.knowledgeBase.push(res);
-        session.consult(res);
-      });
-    fetch("src/prolog-scripts/labirinto.pl")
-      .then((res) => res.text())
-      .then((res) => {
-        this.knowledgeBase.push(res);
-        session.consult(res);
-      });
-  },
+  beforeCreate() {},
   created() {},
   methods: {},
-  computed: {
-    // a computed getter
-    program: function () {
-      return this.knowledgeBase.join("\n");
-    },
-  },
+  computed: {},
 });
--- src/components/prolog-console.vue.js
+++ src/components/prolog-console.vue.js
@@ -1,5 +1,10 @@
+const MAX_RESOLUTION_STEPS = 10000;
+const MODULES = ":- use_module(library(lists)).";
+const session = pl.create(MAX_RESOLUTION_STEPS);
+session.consult(MODULES);
+
 Vue.component("prolog-console", {
-    template: `<div>
+  template: `<div>
   <div class="accordion fixed-bottom" id="accordionInterprete">
     <div class="card">
       <div class="card-header" id="headingOne">
@@ -23,15 +28,15 @@ Vue.component("prolog-console", {
         aria-labelledby="headingOne"
         data-parent="#accordionInterprete"
       >
-        <div class="card-body">
- 
+        <div class="card-body" :key="updater">
           <div id="prolog-console-app" @keydown.ctrl.enter="runProlog()">
             <section class="row">
               <div class="col">
                 <h2>Knowledge Base</h2>
+                <div>Cuurent KB =>{{KB()}}</div>
                 <textarea
                   name="input-7-1"
-                  v-model="program"
+                  v-model="knowledgeBase.program"
                   placeholder="Write Program"
                 ></textarea>
               </div>
@@ -46,8 +51,9 @@ Vue.component("prolog-console", {
               <div class="col">
                <button type="button" class="btn btn-outline-success btn-block" @click="runProlog()" v-on:keypress.ctrl.enter="runProlog()">Run</button>
                <div class="answer">{{answers}}</div>
+               <div class="answer">{{mazeobj.string}}</div>
               </div>
-            </section>   
+            </section>
           </div>
         </div>
       </div>
@@ -55,28 +61,59 @@ Vue.component("prolog-console", {
   </div>
 </div>
 `,
-    props: ["program", "session", "pl"],
+  props: ["mazeobj"],
 
-    data: () => ({
-        query: "",
-        answers: "",
-        libs: ":- use_module(library(lists))",
-    }),
-    created() {
+  data: () => ({
+    query: "",
+    answers: "",
+    libs: ":- use_module(library(lists))",
+    knowledgeBase: { program: "" },
+    updater: 0,
+  }),
+  beforeCreate() {
+    fetch("src/prolog-scripts/labirinto.pl")
+      .then((res) => res.text())
+      .then((res) => {
+        this.knowledgeBase["actions"] = res;
+        this.updater++;
+      });
+  },
+  created() {},
+  methods: {
+    runProlog() {
+      this.answers = "";
+      this.query = this.query.trim();
+      if (this.query.substr(-1) !== ".") {
+        this.query += ".";
+      }
+
+      this.answers = this.getAnswerlist(this.query).join("\n");
     },
-    methods: {
-        runProlog() {
-            this.answers = "";
-            this.query = this.query.trim();
-            if (this.query.substr(-1) !== ".") {
-                this.query += ".";
-            }
-            // const session = pl.create(1000);
-            // session.consult(this.libs + " " + this.program);
-            this.session.query(this.query);
-            session.answers((x) => {
-                this.answers = this.answers.concat(this.pl.format_answer(x), "\n");
-            });
-        },
+
+    getAnswerlist(query) {
+      // console.log("kb", this.KB());
+
+      const session = pl.create();
+      session.consult(this.KB());
+      session.query(query);
+      let answers_list = [];
+      let current_answer = true;
+      while (current_answer) {
+        session.answer((ans) => {
+          current_answer = ans;
+          answers_list.push(pl.format_answer(ans));
+        });
+      }
+      return answers_list;
     },
+
+    KB() {
+      // console.log(Object.entries(this.knowledgeBase));
+
+      return Object.entries(this.knowledgeBase)
+        .map(([key, value]) => value)
+        .join("\n");
+    },
+  },
+  computed: {},
 });
