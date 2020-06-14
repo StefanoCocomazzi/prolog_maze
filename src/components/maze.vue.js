70920ad7164474feda710ab2c00f43c9533665db
Stefano Cocomazzi <stefano.cocomazzi@edu.unito.it>
2020-06-14 11:47:51 +0200

    changed layout

--- src/components/maze.vue.js
+++ src/components/maze.vue.js
@@ -1,8 +1,9 @@
 Vue.component("app-maze", {
-  template: `
-  <div class="container" @mouseup="clicking = false">
-    <div class="row pt-3 justify-content-center">
-    <div class="col-md-3 col-xs-6 pb-2">
+    template: `
+  <div class="col-md-9" @mouseup="clicking = false">
+    <div class="row pt-1 m-0">
+    <div class="col-md-1 col-xs-6 p-1">
+        <strong>Rows</strong>
         <input
           v-model="rows"
           type="number"
@@ -10,7 +11,8 @@ Vue.component("app-maze", {
           placeholder="Rows"
         />
       </div>
-      <div class="col-md-3 col-xs-6 pb-2">
+      <div class="col-md-1 col-xs-6 p-1">
+        <strong>Columns</strong>
         <input
           v-model="columns"
           type="number"
@@ -18,9 +20,8 @@ Vue.component("app-maze", {
           placeholder="Columns"
         />
       </div>
-    </div>
-    <div class="row justify-content-center">
-       <div class="col-md-6 pb-1">
+       <div class="col p-1">
+        <strong>Draw Maze</strong>
         <div class="btn-group btn-group-toggle btn-block" data-toggle="buttons">
           <label class="btn btn-secondary">
             <input @click="editMode='empty'"  type="radio" name="options" /> Empty
@@ -37,21 +38,23 @@ Vue.component("app-maze", {
           </label>
         </div>
       </div>
-    </div>
-    <div class="row justify-content-center">
-       <div class="col-md-6 pb-1">
+       <div class="col p-1">
+        <strong>Utilities</strong>
         <div class="btn-group btn-group-toggle btn-block" data-toggle="buttons">
           <label class="col  btn btn-secondary">
             <input @click="generateMaze()"  type="radio" name="options" /> Random Maze
           </label>
           <label class="col btn btn-secondary">
             <input @click="resetMaze()"  type="radio" name="options" /> Reset
+          </label>          
+          <label class="col btn btn-secondary">
+            <input @click="resetMaze()"  type="radio" name="options" /> Clean
           </label>
 
         </div>
       </div>
     </div>
-    <div class="row justify-content-center _table-container" >
+    <div class="row justify-content-center align-content-center _table-container h-75 mt-2" ref="tc">
         <table :key="updater">
           <tr v-for="row in Number(rows)" :key="row">
             <td
@@ -71,100 +74,108 @@ Vue.component("app-maze", {
   </div>
 
   `,
-  props: ["mazeobj"],
-  data: () => ({
-    MAX_ROWS: 25,
-    MAX_COLS: 25,
-    columns: 10,
-    rows: 10,
-    maze: [],
-    editMode: "wall", // empty, start, goal
-    updater: 0,
-    clicking: false,
-  }),
-  created() {
-    this.resetMaze();
-    this.generateMaze();
-    window.addEventListener("mousedown", this.startClicking);
-    window.addEventListener("mouseup", this.stopClicking);
-    eventBus.$on("find-path-clicked", () => this.findPath());
-  },
-  beforeDestroyed() {
-    window.removeEventListener("mouseup", this.stopClicking);
-    window.removeEventListener("mousedown", this.startClicking);
-  },
-  methods: {
-    stopClicking() {
-      this.clicking = false;
+    props: ["mazeobj"],
+    data: () => ({
+        MAX_ROWS: 100,
+        MAX_COLS: 100,
+        columns: 10,
+        rows: 10,
+        maze: [],
+        editMode: "wall", // empty, start, goal
+        updater: 0,
+        clicking: false,
+    }),
+    created() {
+        this.resetMaze();
+
+        window.addEventListener("mousedown", this.startClicking);
+        window.addEventListener("mouseup", this.stopClicking);
+        eventBus.$on("find-path-clicked", () => this.findPath());
     },
-    startClicking() {
-      this.clicking = true;
+    mounted() {
+        const width = this.$refs.tc.clientWidth;
+        const height = this.$refs.tc.clientHeight;
+        this.columns = Math.floor(width / 32);
+        this.rows = Math.floor(height / 32);
+        this.resetMaze();
+        this.generateMaze();
     },
-    hovering(row, col) {
-      if (this.clicking) {
-        this.setState(row, col);
-      }
+    beforeDestroyed() {
+        window.removeEventListener("mouseup", this.stopClicking);
+        window.removeEventListener("mousedown", this.startClicking);
     },
-    setState(row, col) {
-      this.maze[row][col] = this.editMode;
-      this.updater++;
+    methods: {
+        stopClicking() {
+            this.clicking = false;
+        },
+        startClicking() {
+            this.clicking = true;
+        },
+        hovering(row, col) {
+            if (this.clicking) {
+                this.setState(row, col);
+            }
+        },
+        setState(row, col) {
+            this.maze[row][col] = this.editMode;
+            this.updater++;
+        },
+        getCellState(row, col) {
+            return this.maze[row][col];
+        },
+        generateCode() {
+            let res = [];
+            res.push("columns(" + this.columns + ").");
+            res.push("rows(" + this.rows + ").");
+            let start = "";
+            let goals = [];
+            let walls = [];
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.columns; j++) {
+                    if (this.maze[i][j] === "start") {
+                        start = "start(pos(" + (i + 1) + "," + (j + 1) + ")).";
+                    } else if (this.maze[i][j] === "goal") {
+                        goals.push("goal(pos(" + (i + 1) + "," + (j + 1) + ")).");
+                    } else if (this.maze[i][j] === "wall") {
+                        walls.push("wall(pos(" + (i + 1) + "," + (j + 1) + ")).");
+                    }
+                }
+            }
+            const bound = `max_bound(${this.rows * this.columns}).`;
+            return res
+                .concat(bound)
+                .concat(start)
+                .concat(goals)
+                .concat(walls)
+                .join("\n");
+        },
+        findPath() {
+            eventBus.$emit("find-path", this.generateCode());
+        },
+        resetMaze() {
+            new Array(this.MAX_ROWS).fill([]).forEach((el, i) => {
+                this.maze[i] = new Array(this.MAX_COLS).fill("empty");
+            });
+            this.updater++;
+        },
+        generateMaze() {
+            for (let row = 0; row < this.rows; row++) {
+                for (let col = 0; col < this.columns; col++) {
+                    if (Math.random() < 0.2) {
+                        this.maze[row][col] = "wall";
+                    } else {
+                        this.maze[row][col] = "empty";
+                    }
+                }
+            }
+            let row = Math.floor(Math.random() * this.rows);
+            let col = Math.floor(Math.random() * this.columns);
+            this.maze[row][col] = "goal";
+            row = Math.floor(Math.random() * this.rows);
+            col = Math.floor(Math.random() * this.columns);
+            this.maze[row][col] = "start";
+            this.updater++;
+        },
     },
-    getCellState(row, col) {
-      return this.maze[row][col];
-    },
-    generateCode() {
-      let res = [];
-      res.push("columns(" + this.columns + ").");
-      res.push("rows(" + this.rows + ").");
-      let start = "";
-      let goals = [];
-      let walls = [];
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.columns; j++) {
-          if (this.maze[i][j] === "start") {
-            start = "start(pos(" + (i + 1) + "," + (j + 1) + ")).";
-          } else if (this.maze[i][j] === "goal") {
-            goals.push("goal(pos(" + (i + 1) + "," + (j + 1) + ")).");
-          } else if (this.maze[i][j] === "wall") {
-            walls.push("wall(pos(" + (i + 1) + "," + (j + 1) + ")).");
-          }
-        }
-      }
-      const bound = `max_bound(${this.rows * this.columns}).`;
-      return res
-        .concat(bound)
-        .concat(start)
-        .concat(goals)
-        .concat(walls)
-        .join("\n");
-    },
-    findPath() {
-      eventBus.$emit("find-path", this.generateCode());
-    },
-    resetMaze() {
-      new Array(this.MAX_ROWS).fill([]).forEach((el, i) => {
-        this.maze[i] = new Array(this.MAX_COLS).fill("empty");
-      });
-      this.updater++;
-    },
-    generateMaze() {
-      for (let row = 0; row < this.rows; row++) {
-        for (let col = 0; col < this.columns; col++) {
-          if (Math.random() < 0.2) {
-            this.maze[row][col] = "wall";
-          } else {
-            this.maze[row][col] = "empty";
-          }
-        }
-      }
-      let row = Math.floor(Math.random() * this.rows);
-      let col = Math.floor(Math.random() * this.columns);
-      this.maze[row][col] = "goal";
-      row = Math.floor(Math.random() * this.rows);
-      col = Math.floor(Math.random() * this.columns);
-      this.maze[row][col] = "start";
-      this.updater++;
-    },
-  },
-  computed: {},
+    computed: {},
 });
---
+++ src/components/sidebar.vue.js
@@ -0,0 +1,96 @@
+Vue.component("pms-toolbar", {
+    template: `
+<div class="navbar navbar-expand-md col-md-3 border-right border-secondary">
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+  <div class="collapse navbar-collapse row min-vh-100 align-content-start" id="navbarSupportedContent">
+    <div class="col-12 mt-1 mb-1">
+      <h5>Solving Strategy</h5>
+      <div class="btn-group btn-group-toggle btn-block" data-toggle="buttons">
+        <label class="col btn btn-outline-primary active">
+          <input
+            @click="setStrategy('dfs')"
+            type="radio"
+            name="options"
+            checked
+          />
+          DFS
+        </label>
+        <label class="col btn btn-outline-primary">
+          <input @click="setStrategy('bfs')" type="radio" name="options" /> BFS
+        </label>
+        <label class="col btn btn-outline-primary">
+          <input @click="setStrategy('iddfs')" type="radio" name="options" />
+          IDDFS
+        </label>
+        <label class="col btn btn-outline-primary">
+          <input @click="setStrategy('idastar')" type="radio" name="options" />
+          IDA*
+        </label>
+        <label class="col btn btn-outline-primary">
+          <input @click="setStrategy('astar')" type="radio" name="options" /> A*
+        </label>
+      </div>
+    </div>
+    <div class="col-12 mt-1 mb-1">
+      <h5>Heuristic</h5>
+      <div class="btn-group btn-group-toggle btn-block" data-toggle="buttons">
+        <label class="col btn btn-outline-primary active">
+          <input
+            @click="setStrategy('dfs')"
+            type="radio"
+            name="options"
+            checked
+          />
+          Manhattan Distance
+        </label>
+        <label class="col btn btn-outline-primary">
+          <input @click="setStrategy('bfs')" type="radio" name="options" /> Euclidean Distance
+        </label>
+      
+      </div>
+    </div>
+    <div class="col-12 mt-1 mb-1">
+        <h5>Travel Speed</h5>
+        <div class="input-group">
+            <input @change="setTravelSpeed"
+              v-model="speed"
+              type="number"
+              class="form-control"
+              placeholder="Travel Speed"
+            />
+            <div class="input-group-append">
+                <span class="input-group-text">ms</span>
+            </div>    
+        </div>
+    </div>
+    <div class="col-12 mt-1 mb-1">
+      <button class="btn btn-success btn-block" @click="findPath()">
+        Find Path
+      </button>
+      <button class="btn btn-primary btn-block" @click="findPath()">
+        Retrace Solution
+      </button>
+    </div>
+  </div>
+</div>
+
+  `,
+    data() {
+        return {
+            speed: 100
+        }
+    },
+    methods: {
+        setStrategy(strategy) {
+            eventBus.$emit("set-strategy", strategy);
+        },
+        findPath() {
+            eventBus.$emit("find-path-clicked");
+        },
+        setTravelSpeed() {
+            eventBus.$emit("set-travel-speed", this.speed);
+        }
+    },
+});
